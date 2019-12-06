d736ed93c24ce78ef15c6fadcbcdff66809cd92c
Tim <tim.policar@gmail.com>
2019-12-06 12:05:24 +0100

    initial commit, fragments design

---
+++ app/src/main/java/com/timpolicar/jetpacknavigationcomponenttutorial/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.timpolicar.jetpacknavigationcomponenttutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Navigation Component
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
